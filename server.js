f2273c72101ba0ab72b2fc016b35fe1fb390f7f1
Melissa Wagasky <melissawagasky@gmail.com>
2018-02-27 19:48:06 -0700

    Add socket connection and collect audio on the server side

--- server.js
+++ server.js
@@ -4,9 +4,15 @@ const SpeechToTextV1 = require('watson-developer-cloud/speech-to-text/v1');
 const fs = require('fs');
 const apiKey = require('./apiKey.js');
 const { password, username } = apiKey;
-const app = express()();
-const server = require('http').createServer(app)
-const io = require('socket.io')(server);
+const app = express();
+const http = require('http')
+const socketIO = require('socket.io')
+const server = http.createServer(app)
+const io = socketIO(server);
+const ss = require('socket.io-stream');
+const L16 = require('./webaudio-l16-stream');
+const inspect = require('inspect-stream');
+const port = 5000
 
 const speechToText = new SpeechToTextV1({
   username: username,
@@ -16,16 +22,15 @@ const speechToText = new SpeechToTextV1({
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
-
 app.use(function(request, response, next) {
   response.header("Access-Control-Allow-Origin", "*");
   response.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
 
-app.get('/', async (request, response, next) => {
-  response.send('hello world')
-});
+// app.get('/', async (request, response, next) => {
+//   response.send('hello world')
+// });
 
 const params = {
   audio: fs.createReadStream('audio-file-3.flac'),
@@ -37,32 +42,40 @@ const params = {
   // keywords_threshold: 0.5
 }
 
-app.post('/checkAudioText', async (request, response, next) => {
+const recognizeStream = app.post('/checkAudioText', async (request, response, next) => {
 
   console.log('request:', request.body)
-  await speechToText.recognize(params, function(error, transcript) {
+  await speechToText.recognize({
+    audio: fs.createReadStream('audio-file-3.flac'),
+    content_type: 'audio/wav',
+    timestamps: true,
+    word_confidence: true,
+  }, function(error, transcript) {
     if (error) {
       console.log('Error:', error);
     } else {
-      console.log('here i am take 2!')
+      console.log('here i am!')
       console.log(JSON.stringify(transcript.results, null, 2));
     }
   });
-
 })
 
+const l16Stream = new L16({ writableObjectMode: false });
 
 io.on('connection', (socket) => {
-  socket.emit('news', { hello: 'world' });
-  socket.on('my other event', (data) => {
-    console.log(data)
+  console.log('something')
+
+  ss(socket).on('audio', (stream) => {
+    console.log('audio stream')
+    stream.pipe(l16Stream).pipe(inspect()).pipe(recognizeStream)
   })
 })
 
-server.listen(5000, () => {
-  console.log('express running localhost5000')
-})
+// erroring out at l16Stream
 
+server.listen(port, () => {
+  console.log(`express running port ${port}`)
+})
 
 
 
